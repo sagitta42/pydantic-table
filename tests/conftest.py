32cbd5125254ff45ee5603092143aa38b63e72b1
sagitta42 <mariia.redchuk@gmail.com>
2026-08-27 20:39:34 +0200

    remove direct example model import in conftest to avoid breaking test suite

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ import os
 
 from dotenv import dotenv_values
 
-from examples.models import ExampleTable
+# from examples.models import ExampleTable
 
 env_config = dotenv_values()
 is_debug = env_config.get("DEBUG", "").lower() in ("true", "1")
@@ -22,19 +22,19 @@ PATH_TO_ASSETS = Path(os.path.dirname(__file__))
 PATH_TO_CONFIGS = PATH_TO_ASSETS / "configs"
 
 
-def get_example_model(filename: str) -> ExampleTable:
-    model_config_path = PATH_TO_CONFIGS / f"{filename}.json"
-    with open(model_config_path) as f:
-        dataset_info = ExampleTable(**json.load(f))
-    return dataset_info
+# def get_example_model(filename: str) -> ExampleTable:
+#     model_config_path = PATH_TO_CONFIGS / f"{filename}.json"
+#     with open(model_config_path) as f:
+#         dataset_info = ExampleTable(**json.load(f))
+#     return dataset_info
 
 
-def get_example_test_case(filename: str = "test_model"):
-    example_model = get_example_model(filename)
-    ret = [pytest.param(example_model, id=filename)]
-    return ret
+# def get_example_test_case(filename: str = "test_model"):
+#     example_model = get_example_model(filename)
+#     ret = [pytest.param(example_model, id=filename)]
+#     return ret
 
 
-@pytest.fixture(params=get_example_test_case())
-def test_case_example(request) -> ExampleTable:
-    return request.param
+# @pytest.fixture(params=get_example_test_case())
+# def test_case_example(request) -> ExampleTable:
+#     return request.param
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,6 +1,23 @@
+import json
+
+from pydantic import Field
+
 from pydantic_table.logger import logg
+from pydantic_table.table_model.model import TableModel
+from tests.conftest import PATH_TO_CONFIGS
 
 
-def test_model(test_case_example):
+def test_model():
+    filename = "test_model"
+
+    class ExampleTable(TableModel, table_name="examples", primary_keys=["id"]):
+        id: int = Field(description="ID")
+        name: str = Field(description="Name")
+        value: float = Field(description="Value")    
+
+    model_config_path = PATH_TO_CONFIGS / f"{filename}.json"
+    with open(model_config_path) as f:
+        model = ExampleTable(**json.load(f))
+
     logg.debug("Example table")
-    logg.debug(test_case_example)
+    logg.debug(model)
