fcb1cf0ce06b19f79dcf9f6465a34c2329aeb165
sagitta42 <mariia.redchuk@gmail.com>
2026-08-28 21:10:16 +0200

    minor: nullable column in test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -15,7 +15,7 @@ def test_model():
     class ExampleTable(TableModel, table_name="examples"):
         id: int = ColumnField(description="ID", primary_key=True)
         name: str = ColumnField(description="Name")
-        value: float = ColumnField(description="Value")
+        value: float = ColumnField(description="Value", nullable=True)
 
     model_config_path = PATH_TO_CONFIGS / f"{filename}.json"
     with open(model_config_path) as f:
