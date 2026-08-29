477c6e4d057a160c03fb7c76aa8ee7fab2568447
sagitta42 <mariia.redchuk@gmail.com>
2026-08-29 10:54:58 +0200

    bugfix: base import

--- src/pydantic_table/sqlalchemy/__init__.py
+++ src/pydantic_table/sqlalchemy/__init__.py
@@ -1,4 +1,3 @@
 from pydantic_table.sqlalchemy.base import BaseMeta as BaseMeta
-from pydantic_table.sqlalchemy.base import Base as Base
 from pydantic_table.sqlalchemy.column import Column as Column
 from pydantic_table.sqlalchemy.table import Table as Table
