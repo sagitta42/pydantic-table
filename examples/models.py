68e39d7a8fba434792c4c61f02eef80dd9919c73
sagitta42 <mariia.redchuk@gmail.com>
2026-08-31 23:54:14 +0200

    update example model

--- examples/models.py
+++ examples/models.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import DeclarativeBase
 from pydantic import Field
 
 
-from pydantic_table import TableModel
+from pydantic_table import ColumnField, TableModel
 from pydantic_table.sqlalchemy import BaseMeta
 
 
@@ -10,10 +10,10 @@ class Base(DeclarativeBase):
     pass
 
 
-class ExampleTable(TableModel, table_name="examples", primary_keys=["id"]):
-    id: int = Field(description="ID")
-    name: str = Field(description="Name")
-    value: float = Field(description="Value")
+class ExampleTable(TableModel, table_name="examples"):
+    id: int = ColumnField(description="ID", primary_key=True)
+    name: str = ColumnField(description="Name")
+    value: float = ColumnField(description="Value", nullable=True)
 
 
 class ExampleTableBase(Base, metaclass=BaseMeta, model=ExampleTable):
